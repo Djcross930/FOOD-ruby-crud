b49f43dd567bd5009a40b0afbc09592bbe36425a
Dan Cross <dancross96@gmail.com>
2022-08-25 21:19:42 -0500

    added show action to foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,4 +3,14 @@ class FoodsController < ApplicationController
     foods = Food.all
     render json: foods.as_json
   end
+
+  def show
+    food = Food.find_by(id: params[:id])
+    render json: food.as_json
+  end
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/foods" => "foods#index"
+  get "/foods/:id" => "foods#show"
 end
